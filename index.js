7b3e005cfc6f0c882a504697241a316fcc7a71d2
artorias abysswalker <artorias@artoriass-MacBook-Pro.local>
2016-05-23 11:54:28 +1200

    Updates

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
 
   console.log(data.body)
-  // var sat = data.body[0].name.replace('/[|]/g', "")
-  // console.log(sat)
-  console.log(typeof data.body)
-  console.log(JSON.parse(data.body))
-
-  
-
   document.body.innerHTML = example({ name: "Space" });
 })
 
