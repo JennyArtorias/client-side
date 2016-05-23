c32018f77a786b98e87252a5a0798d76234dbb0c
artorias abysswalker <artorias@artoriass-MacBook-Pro.local>
2016-05-23 12:22:58 +1200

    Updates

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if (err) console.log(err) // do something
 
   console.log(data.body)
-  console.log(typeof data.body)
-  var sat = JSON.parse(data.body.replace('/[|]/g', ""))
-  console.log(sat[0])
-  console.log(sat[0].name)
-  var name1 = sat[0].name
+  // console.log(typeof data.body)
+  var sat1 = JSON.parse(data.body.replace('/[|]/g', ""))
+  // console.log(sat[0])
+  // console.log(sat[0].name)
+  var name1 = sat1[0].name
 
 
 
 
   document.body.innerHTML = example({ name: name1 });
 })
+
+xhr.get('https://api.wheretheiss.at/v1/satellites', function (err, data) {
+  if (err) console.log(err)
+
+    console.log(data.body)
+
+    var sat2 = JSON.parse(data.body.replace('/[|]/g', ""))
+    var id1 = sat2[0].id1
+
+    document.body.innerHTML = example({ id: id1 });
+
+})
+
+
+
+
+
 
